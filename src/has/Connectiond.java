67be1acef2a7ccae69241b1435da717d930a2d3c
MAHANKALI SAKETH <MAHANKALI SAKETH@HP>
2015-04-02 18:52:15 +0530

    repository initiated

---
+++ src/has/Connectiond.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package has;
+ import java.sql.*;
+/**
+ *
+ * @author MAHANKALI SAKETH
+ */
+public class Connectiond {
+    
+        static String url = "jdbc:mysql://localhost:3306/dbhas";
+        static String password = "89878";
+        static String user = "root";
+        public static void main(String[] args) {
+            
+        
+    }
+        static Connection getconn() throws SQLException
+        {
+            System.out.println("456");
+            Connection con = DriverManager.getConnection(url, user, password);
+            System.out.println("123");
+            return con;
+        }
+    
+    
+}
