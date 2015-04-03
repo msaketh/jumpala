f96376a05478a263da5c7e2e16703144bbcf71e8
MAHANKALI SAKETH <MAHANKALI SAKETH@HP>
2015-04-03 17:52:06 +0530

    17:51

--- src/has/ManagerFunctions.java
+++ src/has/ManagerFunctions.java
@@ -5,25 +5,18 @@
  */
 package has;
 
-import java.sql.*;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 /**
  *
  * @author MAHANKALI SAKETH
  */
-public class Login extends javax.swing.JFrame {
+public class ManagerFunctions extends javax.swing.JFrame {
 
     /**
-     * Creates new form Login
-   */
-    public Login() {
-        
+     * Creates new form ManagerFunctions
+     */
+    public ManagerFunctions() {
         initComponents();
     }
-    
-    
 
     /**
      * This method is called from within the constructor to initialize the form.
@@ -36,34 +29,34 @@ public class Login extends javax.swing.JFrame {
 
         jLabel1 = new javax.swing.JLabel();
         jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
-        jButton3 = new javax.swing.JButton();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+
+        jLabel1.setIcon(new javax.swing.ImageIcon("C:\\Users\\MAHANKALI SAKETH\\Desktop\\disneyrooms.png")); // NOI18N
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setBackground(new java.awt.Color(204, 204, 0));
+        setForeground(new java.awt.Color(204, 204, 0));
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jLabel1.setText("WELCOME TO HOTEL AUTOMATION SOFTWARE");
-        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(99, 11, -1, -1));
-
-        jButton1.setText("LOGIN AS MANAGER");
+        jButton1.setBackground(new java.awt.Color(102, 204, 0));
+        jButton1.setText("PROCEED");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton1ActionPerformed(evt);
             }
         });
-        getContentPane().add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(114, 60, 185, -1));
+        getContentPane().add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(640, 20, 140, 30));
+        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 50, -1, -1));
 
-        jButton2.setText("LOGIN AS RECEPTIONIST");
-        getContentPane().add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(114, 113, 185, -1));
-
-        jButton3.setText("LOGIN AS CATERING SERVICES");
-        getContentPane().add(jButton3, new org.netbeans.lib.awtextra.AbsoluteConstraints(114, 167, -1, -1));
+        jLabel3.setIcon(new javax.swing.ImageIcon("C:\\Users\\MAHANKALI SAKETH\\Desktop\\Manager.jpg")); // NOI18N
+        getContentPane().add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 910, 560));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-
+        // TODO add your handling code here:
     }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
@@ -83,43 +76,28 @@ public class Login extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ManagerFunctions.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ManagerFunctions.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ManagerFunctions.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ManagerFunctions.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
-        
-            /* Create and display the form */
-            try
-            {
-               Statement stmt = null;
-               Connection con = Connectiond.getconn();
-               stmt = con.createStatement(); 
-                String sql = "CREATE TABLE login" +
-                   "(username VARCHAR(50), " +
-                   " password VARCHAR(50), " +
-                     "PRIMARY KEY(username))";
-                stmt.executeUpdate(sql);
-                
-            }
-            catch (Exception ex) {
-              Logger.getLogger(Login.class.getName()).log(Level.SEVERE, null, ex);
-            }
-            java.awt.EventQueue.invokeLater(new Runnable() {
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Login().setVisible(true);
+                new ManagerFunctions().setVisible(true);
             }
         });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
-    private javax.swing.JButton jButton3;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
     // End of variables declaration//GEN-END:variables
 }
